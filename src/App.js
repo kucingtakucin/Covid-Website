b813b377899cfc6815acb5e8418115363aa76fc2
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-05-15 21:59:45 +0700

    Mengubah file App.js, dan mengubah struktur direktori

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <header>
+                    <nav>
+                        Ini buat nav
+                    </nav>
+                    Ini buat header
+                </header>
+                <main>
+                    Ini buat main content
+                </main>
+                <footer>
+                    Ini buat footer
+                </footer>
+            </React.Fragment>
+        )
+    }
 }
 
 export default App;
