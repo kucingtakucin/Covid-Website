83c6f940bd9dd8ef3ac6545f94aee231b7cd16b0
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-05-23 11:51:53 +0700

    Mengecek si now nya bisa engga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import '../styles/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App({ Component, pageProps }) {
     return <Component {...pageProps} />
